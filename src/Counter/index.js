e3873dad8c397261a61bcd004072b99d6128c233
Eric Laursen <ericjlaursen@gmail.com>
2019-05-16 20:55:57 -0400

    updates code layout

--- src/Counter/index.js
+++ src/Counter/index.js
@@ -3,7 +3,9 @@ import React, { useState } from 'react'
 function Counter(){
 	const [count, setCount] = useState(0)
 	const inc = () => setCount(count + 1)
-	return <button onClick={inc}>{count}</button>
+	return <button 
+		className="test-button" 
+		onClick={inc}>{count}</button>
 }
 
 export default Counter
