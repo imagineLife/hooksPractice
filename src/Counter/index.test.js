2d6dc28ea343797900cdbb1d0995c7cb96b93c95
Eric Laursen <ericjlaursen@gmail.com>
2019-05-16 20:28:44 -0400

    Functional test fix

--- src/Counter/index.test.js
+++ src/Counter/index.test.js
@@ -6,4 +6,17 @@ describe('<Count />', () => {
 	it('renders', () => {
 		shallow(<Count />)
 	})
+	describe('interacts:', () => {
+		it('increments 1 when clicked', () => {
+			const count = shallow(<Count />);
+			const btn = count.find('button');
+			
+			//check initial value
+			expect(btn.text()).toBe("0")
+			
+			//click && re-check
+			btn.simulate('click');
+			expect(count.find('button').text()).toBe("1");
+		})
+	})
 })
