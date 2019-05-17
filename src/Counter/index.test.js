04d7a541255784a5b0dc1214a0159a0084dd30c6
Eric Laursen <ericjlaursen@gmail.com>
2019-05-16 20:08:57 -0400

    functionion shallow render test

--- src/Counter/index.test.js
+++ src/Counter/index.test.js
@@ -1,3 +1,9 @@
 import React from 'react'
 import Count from './index'
+import { configure, shallow } from "enzyme"
 
+describe('<Count />', () => {
+	it('renders', () => {
+		shallow(<Count />)
+	})
+})
