54ddb412b55f9b87d5fa46b4aec294b048b1809b
Eric Laursen <ericjlaursen@gmail.com>
2019-05-16 20:56:05 -0400

    init hook file

---
+++ src/CountHook.js
@@ -0,0 +1,15 @@
+import react, { useState } from 'react'
+
+//custom 'simple' hook
+export function useCounter(){
+	const [count, setCount] = useState(0)
+	const inc = () => setCount(count + 1)
+	return {count, inc}
+}
+
+//more complex hook
+export function useCounterComplex(initialState){
+	const [count, setCount] = useState(initialState)
+	const inc = () => setCount(count + 1)
+	return {count, inc}
+}
